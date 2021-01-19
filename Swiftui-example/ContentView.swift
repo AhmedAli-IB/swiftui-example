a80020156ba2d5581afdf55af688e9f6fae5f1ba
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-01-19 16:22:24 +0200

    Add header image

--- Swiftui-example/ContentView.swift
+++ Swiftui-example/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello!")
-            .padding()
+        VStack(alignment: .center) {
+            ZStack {
+                Image("Mask")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                   
+                HStack {
+                    Image("ic_edit")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    Image("ic_back-arrow")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }.padding()
+            } .frame(width: UIScreen.main.bounds.size.width, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Spacer()
+   
+        }
     }
 }
 
